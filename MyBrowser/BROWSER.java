8f21ae6cdfa2aba5e80c1b0f98f796079e27ac13
kulde <kuldeeps131196@gmail.com>
2017-10-09 21:10:35 +0530

    MyBrowser Uploaded

---
+++ MyBrowser/BROWSER.java
@@ -0,0 +1,74 @@
+               /*------------------------------------------------------------------------------*/
+               //          PROJECT FOR  DEVELOPEMENT OF A WEB BROWSER
+               /*------------------------------------------------------------------------------*/   
+
+  package browser;
+
+/*------------------------------------------------------------------------------*/
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+/*------------------------------------------------------------------------------*/
+
+public class BROWSER extends JFrame
+{
+   private JTextField addressBar;
+   private JEditorPane display; 
+   
+   //constructor
+   public BROWSER()
+   {
+      super("KULDEEP BROWSER");
+      
+      addressBar =new JTextField("enter a URL address!");
+      addressBar.addActionListener
+       (
+          new ActionListener() 
+           {
+              public void actionPerformed(ActionEvent event)
+               {
+                 loadCrap(event.getActionCommand());                        
+               }                   
+           }                                                
+       );  
+      
+      add(addressBar, BorderLayout.NORTH);
+       
+      display =new JEditorPane();
+      display.setEditable(false);
+      display.addHyperlinkListener
+       (
+         new HyperlinkListener()
+          {  
+            public void hyperlinkUpdate(HyperlinkEvent event)
+             {
+               if(event.getEventType()==HyperlinkEvent.EventType.ACTIVATED)
+               {
+                 loadCrap(event.getURL().toString());
+               }             
+             }                         
+          }                            
+       );   
+      
+      add(new JScrollPane(display),BorderLayout.CENTER) ;
+      setSize(500,300);
+      setVisible(true);
+   }
+   
+   //load crap to display on the screen
+    private void loadCrap(String userText)
+    {
+        try {
+              display.setPage(userText);
+              addressBar.setText(userText);
+            }         
+        catch (Exception e) 
+        {
+          JOptionPane.showMessageDialog(this, "Internet Connection or any other unwanted error! ");
+          //System.out.println("ERROR");
+        }
+    
+    }
+}
+     
---
+++ MyBrowser/browserMain.java
@@ -0,0 +1,12 @@
+
+package browser;
+import javax.swing.JFrame;
+
+public class browserMain 
+{
+    public static void main(String args[])
+    {
+      BROWSER br=new BROWSER();
+      br.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     }
+}
